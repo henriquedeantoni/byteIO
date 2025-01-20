d13214017195dd49d7de706136667657c0460f26
henriquedeantoni <henriqdeantoni@gmail.com>
2025-01-19 22:07:35 -0300

    feat: atualizacao program.cs

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,35 +1,41 @@
 ﻿using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
 
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-        var buffer = new byte[1024]; 
-
-        
-        
-        while(numeroDeBytesLidos !=0)
+        using(var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
-        }
+            var numeroDeBytesLidos = -1;
 
-        fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-;  
-        Console.ReadLine();
+            var buffer = new byte[1024]; 
+
+            while(numeroDeBytesLidos !=0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            fluxoDoArquivo.Close();
+        
+            Console.ReadLine();
+        }
     }
 
     static void EscreverBuffer(byte[] buffer )
     {
-        foreach( var myByte in buffer)
-        {
-            Console.Write(myByte);
-            Console.Write(" ");
-        }
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+
+        //foreach ( var myByte in buffer)
+        //{
+        //    Console.Write(myByte);
+        //    Console.Write(" ");
+        //}
     }
 }
