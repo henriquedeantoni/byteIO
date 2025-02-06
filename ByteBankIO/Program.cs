77cdaae59b34feff693895b0b25ada57a83d6752
henriquedeantoni <henriqdeantoni@gmail.com>
2025-02-05 23:58:22 -0300

    feat: criada nova classe

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,18 +5,8 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        
-        var caminhoNovoArquivo = "TextaEscrita.txt";
 
-        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-
-        using (var escritor = new StreamWriter(fluxoDeArquivo))
-        {
-            escritor.Write(false);
-            escritor.Write(true);
-            escritor.Write(45454545454545454);
-        }
-        Console.WriteLine("Aplicação Finalizada");
+        EscritaBinaria();
 
         Console.ReadLine();
     }
